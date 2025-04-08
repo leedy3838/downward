cba9e3a5e7726847d75579dd499a079e20430f24
leedy3838 <leedy3838@naver.com>
2025-04-08 15:51:58 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    pod_name = os.environ.get("POD_NAME", "Unknown")
+    node_name = os.environ.get("NODE_NAME", "Unknown")
+    pod_namespace = os.environ.get("POD_NAMESPACE", "Unknown")
+
+    return f"Container EDU | POD Working : {pod_name} | nodename : {node_name} | namespace : {pod_namespace}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
